5b3174306544d77a55a18572e494ab1e39e9d9bc
Bombaric <ceciliabombari12@gmail.com>
2025-08-24 11:46:16 +0200

    simulations on spacings

---
+++ src/finrmt/spectra.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#r statistics of consecutive eigenvalues
+
+def r_statistics(eig_sorted):
+    r_stats = []
+    for i in range(1, len(eig_sorted)-1):
+        delta_n = eig_sorted[i] - eig_sorted[i-1]
+        delta_n_plus_1 = eig_sorted[i+1] - eig_sorted[i]
+        r = min(delta_n, delta_n_plus_1) / max(delta_n, delta_n_plus_1)
+        r_stats.append(r)
+        mean_r = sum(r_stats) / len(r_stats)
+    return mean_r

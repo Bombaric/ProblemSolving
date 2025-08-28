a4271bc99e4132330d7c0163008d1fb676f6eaf2
Bombaric <ceciliabombari12@gmail.com>
2025-08-28 17:31:44 +0200

    experiments on MP and r spacing

--- src/finrmt/rmt.py
+++ src/finrmt/rmt.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # Functions for random matrix theory: compute Marčenko–Pastur bounds for sample covariance eigenvalues
-def mp_bounds(N, T, sigma2=1.0):
+import numpy as np
+def mp_bounds(N, T, sigma2):
  if N > T:
     raise ValueError("N must be less than or equal to T")
  #return the bounds for the eigenvalues
  c = N / T
- lambda_min = sigma2 * (1 - c)**2
- lambda_max = sigma2 * (1 + c)**2
+ q = np.sqrt(c)
+ lambda_min = sigma2 * (1 - q)**2
+ lambda_max = sigma2 * (1 + q)**2
  return (lambda_min, lambda_max)
-#example usage
-fun = mp_bounds(10, 100)
-print("the bounds are: ", fun)
--- src/finrmt/spectra.py
+++ src/finrmt/spectra.py
@@ -2,6 +2,7 @@
 #r statistics of consecutive eigenvalues
 
 def r_statistics(eig_sorted):
+
     r_stats = []
     for i in range(1, len(eig_sorted)-1):
         delta_n = eig_sorted[i] - eig_sorted[i-1]
@@ -17,3 +18,17 @@ def ipr(vecs):
         ipr_value = sum(vec**4) / (sum(vec**2)**2)
         ipr.append(ipr_value)
     return ipr
+
+def r_stat(eigs_sort, trim_k):
+    if trim_k > 0:
+        eigs_sort = eigs_sort[trim_k:-trim_k]
+    else:
+        eigs_sort = eigs_sort[:]
+    r_vals = []
+    for i in range(1, len(eigs_sort)-1):
+        delta_n = eigs_sort[i] - eigs_sort[i-1]
+        delta_n_plus_1 = eigs_sort[i+1] - eigs_sort[i]
+        r = min(delta_n, delta_n_plus_1) / max(delta_n, delta_n_plus_1)
+        r_vals.append(r)
+    mean_r = sum(r_vals) / len(r_vals)
+    return mean_r
